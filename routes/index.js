d3888cc1136917dcb1bc08501c3b5831ef19e1ed
MartosAm <adrianvelarex@gmail.com>
2025-05-02 20:56:46 -0600

    subir archivos

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ const userRoutes = require('./userRoutes');
 const authRoutes = require('./authRoutes');
 const seccionRoutes = require('./seccionRoutes');
 const { authMiddleware } = require('../middleware'); // Importa el objeto completo
+const documentoRoutes = require('./documentoRoutes'); // Ajusta la ruta si es necesario
+const multer = require('multer'); // Importa multer para manejar archivos
 
 router.use('/trabajadores', trabajadorRoutes, authMiddleware.verifyToken); // Usa la propiedad .verifyToken
 router.use('/users', userRoutes, authMiddleware.verifyToken); // Usa la propiedad .verifyToken
 router.use('/auth', authRoutes); 
 router.use('/secciones', seccionRoutes, authMiddleware.verifyToken); // Usa la propiedad .verifyToken
+router.use('/documentos', documentoRoutes, authMiddleware.verifyToken); // Usa la propiedad .verifyToken
 
 // Ruta base para verificar que la API funcion
 router.get('/', (req, res) => {
